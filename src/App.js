040d2bd95fb88eefdf7365f30e49739f4581ca10
NathanMuss <nathanmuss@gmail.com>
2019-07-15 15:01:15 -0600

    Display first crafted result on results page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Results from './components/Results';
+import data from './data/items.json';
 
 class App extends Component {
-  handleSearch(e, query) {
+  constructor() {
+    super();
+    this.state = {
+      result: ""
+    }
+  }
+  handleSearch = (e, query) => {
     e.preventDefault();
-    console.log("Searching: " + query);
+    const result = {
+      craftedResults: getCraftedResults(query)
+    }
+    console.log(result);
+    
+    this.setState({
+      result
+    })
   }
 
   render() {
     return (
       <div className="App">
           <NavBar handleSearch={this.handleSearch}/>
-          <Results />
+          <Results data={this.state.result}/>
       </div>
     );
   }
+}
+
+function getCraftedResults(query) {
+  const crafted = data.crafting;
+  const results = crafted.filter((item) => {
+    return item.result.toLowerCase().includes(query.toLowerCase());
+  });
+  return results;
 }
 
 export default App;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,38 +1,59 @@
 import React from 'react';
 import './Results.css';
 
-const Results = () => {
-    return (
-          <div className="row">
-                <div className="result-container container center">
-                    <div className="result-main">
-                        <img src="https://www.placecage.com/200/200" alt=""/>
-                        <div className="result-details">
-                            <ul>
-                                <li>Name: Condensed Carbon</li>
-                                <li>Price: 100000000</li>
-                            </ul>
-                        </div>
-                        <div className="result-recipes row">
-                            <div className="col s6">
-                                <p>Made from:</p>
-                            </div>
-                            <div className="col s6">
-                                <p>Stuff</p>
-                            </div>
-                        </div>
-                        <div className="result-used-in row">
-                            <div className="col s6">
-                                <p>Can Make:</p>
-                            </div>
-                            <div className="col s6">
-                                <p>Things</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>   
+const Results = (props) => {
+    
+    if (!props.data.craftedResults) {
+        return (
+            <div className="container center">
+                <h2>Please search for an item</h2>
             </div>
-    );
+        )
+    } else {
+        const item = props.data.craftedResults[0]
+        const ingredients = [];
+        ingredients.push({
+            ingredient: item.ingredientOne,
+            image: `/images/${item.imgOne}.png`
+        });   
+        if (item.ingredientTwo) {
+            ingredients.push({
+                ingredient: item.ingredientTwo,
+                image: `/images/${item.imgTwo}.png`
+            });
+        } 
+        if (item.ingredientThree) {
+            ingredients.push({
+                ingredient: item.ingredientThree,
+                image: `/images/${item.imgThree}.png`
+            });
+        } 
+        const ingredientView = ingredients.map((curr, i) => {
+            return (
+                <div key={i} className="col s6">
+                    <p>{curr.ingredient}</p>
+                    <img src={curr.image} />
+                </div>
+            )
+        })
+        
+        return (
+            <div className="row">
+                    <div className="result-container container center">
+                        <div className="result-main">
+                            <div className="result-details">
+                                <p>{item.result}</p>
+                                <p>Value: {item.time}</p>
+                                <img src={`/images/${item.imgResult}.png`} alt=""/>
+                            </div>
+                            <div className="result-recipes row">
+                                {ingredientView}
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+        );
+    }
 }
 
 export default Results;
