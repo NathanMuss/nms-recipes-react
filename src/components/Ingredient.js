5e65bba6e15893ac9787a0c18f17eb9ecccad009
NathanMuss <nathanmuss@gmail.com>
2019-07-27 11:39:10 -0600

    Make ingredients link to items, fix some data

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Ingredient.css';
+import { Link } from 'react-router-dom';
+import updateQuery from '../actions/updateQuery';
+import updateResults from '../actions/updateResults';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Ingredient = (props) => {
-    //TODO: Turn this into a link to the item's recipe page
-    return (
-        <div className="center ingredient-card">
-            <img src={props.item.image} alt={props.item.ingredient}/>
-            <p>{props.item.ingredient}  {props.item.ingredientCount ? "x" + props.item.ingredientCount : "" }</p>
-        </div>
-    );
+
+
+class Ingredient extends Component {
+    searchIngredient() {
+        this.props.updateQuery(this.props.item.ingredient.trim());
+        this.props.updateResults(this.props.item.ingredient.trim());
+    }
+    render() {
+        return (
+            <Link to={`/${this.props.item.ingredient}`} onClick={() => {this.searchIngredient()}}>
+                <div className="center ingredient-card">
+                    <img src={this.props.item.image} alt={this.props.item.ingredient}/>
+                    <p>{this.props.item.ingredient}  {this.props.item.ingredientCount ? "x" + this.props.item.ingredientCount : "" }</p>
+                </div>
+            </Link>
+        );
+    }
+
 }
 
-export default Ingredient;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        updateQuery: updateQuery,
+        updateResults: updateResults
+    }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(Ingredient);
