5ddfed5bbe03c8545f1b7a78070967c0f38f4820
NathanMuss <nathanmuss@gmail.com>
2019-08-04 16:21:33 -0600

    Tweak switch alignments

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,14 +2,15 @@ import React from 'react';
 import updateFilter from '../actions/updateFilter';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import './Switch.css';
 
 const Switch = (props) => {
     return (
         <div className="switch col s2 m2 center">
-            <p className="type-label blue-text darken-3">
-                {props.type}
-            </p>
             <label>
+                <p className="type-label blue-text darken-3">
+                    {props.type}
+                </p>
                 <input type="checkbox" checked={props.filter[props.type]} onChange={() => {props.updateFilter(props.filter, props.type)}}></input>
                 <span className="lever"></span>
             </label>
